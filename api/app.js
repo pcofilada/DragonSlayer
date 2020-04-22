205b5a6ef10d57c51df9864ae9914a2f7d53297b
Patrick Ofilada <pcofilada@gmail.com>
2020-04-22 23:21:21 +0800

    Add cors to api

--- api/app.js
+++ api/app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
 var db = require('./db');
+
+app.use(cors());
 
 var AuthController = require('./auth/AuthController');
 app.use('/api/auth', AuthController);
